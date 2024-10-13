a81f5b293cddf1b973567abd52d1c58181757a7d
mohamed-badr555 <mohamedb.555dr@gmail.com>
2024-10-13 09:32:16 +0300

    fix upload2

--- next.config.mjs
+++ next.config.mjs
@@ -1,26 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: "https",
-          hostname: "images.pexels.com",
-        },
-        {
-          protocol: "https",
-          hostname: "static.wixstatic.com",
-        },
-        {
-          protocol: "https",
-          hostname: "people.pic1.co",
-        },
-        {
-          protocol: "https",
-          hostname: "app-uploads-cdn.fera.ai",
-        },
-      ],
-    },
-    output: 'export', // Add this line for static export
-  };
-  
-  export default nextConfig;
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.pexels.com",
+      },
+      {
+        protocol: "https",
+        hostname: "static.wixstatic.com",
+      },
+      {
+        protocol: "https",
+        hostname: "people.pic1.co",
+      },
+      {
+        protocol: "https",
+        hostname: "app-uploads-cdn.fera.ai",
+      },
+    ],
+ 
+  },
+
+};
+
+export default nextConfig;
